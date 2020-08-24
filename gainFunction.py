5490cfc6d2c2fc47b20ad5e569e3630837dc3568
John Appert <appertjt@gmail.com>
2020-08-24 10:37:04 -0700

    Fixed integration issue.  Updated plot method.

--- gainFunction.py
+++ gainFunction.py
@@ -2,42 +2,66 @@ from scipy.integrate import simps
 from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
+plt.style.use('ggplot')
 
 def get_gain_function(dist, payoff_type, edge, cutoff=-3):
     payoff = []
     # concave vs convex:
     if payoff_type == 'concave':
-        new_dist = filter(lambda x: x > cutoff, dist)
-        new_dist = list(new_dist)
+        # get the square of each number in the dist
+        new_dist = [number**2 for number in dist]
+
+        # get the expectation of the new distribution
         expected = mean(new_dist)
         alpha = (1 + edge) * expected
-        lower_bound = cutoff
-        f = np.linspace(0, -1 / cutoff, 100)
+
         for row in new_dist:
-            s = alpha - row ** 2
+            s = alpha - row
             payoff.append(s)
 
     else:
-        expected = mean(dist)
-        alpha = (1 + edge) * expected
-        f = np.linspace(0, 1 / alpha, 100)
-        for row in dist:
-            s = row ** 2 - alpha
+        # get the square of each number in the dist
+        new_dist = [number**2 for number in dist]
+
+        expected = mean(new_dist)
+        alpha = expected * (1 - edge)
+
+        for row in new_dist:
+            s = row - alpha
             payoff.append(s)
+    # cap the payoff function
+    payoff = filter(lambda z: z > cutoff, payoff)
+    payoff = list(payoff)
+
+    f = np.linspace(0, 0.35, 100)
 
     # get gain function
     g_list = []
     for i in f:
         int_vector = []
-        for j in new_dist:
+        for j in payoff:
             obs = np.log(1 + i * j)
             int_vector.append(obs)
-        gain = simps(int_vector, new_dist)
+        gain = simps(int_vector)
         g_list.append(gain)
 
     return (f, g_list)
 
 
-def plot_gain_function(f, g_list):
-    plt.plot(f, g_list)
+def plot_gain_function(gain1, gain2, label1, label2):
+    plt.plot(gain1[0], gain1[1], label=label1)
+    plt.plot(gain2[0], gain2[1], label=label2)
+    plt.title("Gain function for Concave and Convex Bets")
+    plt.legend()
+    plt.savefig('gainFunctions.png')
     plt.show()
+
+
+if __name__ == '__main__':
+    random_state=np.random.RandomState(seed=42)
+    mu = 0
+    sigma = 1
+    s = random_state.normal(mu, sigma, 1000)
+    x1 = get_gain_function(s, 'convex', 0.05)
+    x2 = get_gain_function(s, 'concave', 0.05)
+    plot_gain_function(x1, x2, "Convex", "Concave")
